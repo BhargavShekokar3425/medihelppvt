e9d6906e9f22f6acb3895dcc4164c6ebc0648263
Ishan Bhambare <b23cs1020@iitj.ac.in>
2025-04-18 05:31:48 +0530

    Appointment page

---
+++ src/pages/Appointments.js
@@ -0,0 +1,14 @@
+// Appointments.js under pages
+
+
+import AppointmentScheduler from '../components/AppointmentScheduler';
+function Appointments() {
+    return (
+      <div>
+        <h2>Appointments Page</h2>
+        <AppointmentScheduler />
+      </div>
+    );
+  }
+  
+  export default Appointments;
